9877b0acd6847d394ebed3b7ba1ab357bf3662fb
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2020-05-27 16:05:07 -0700

    Updated for returning cycle node

--- algorithms-bank/src/FloydTortoiseAndHareAlgorithm.java
+++ algorithms-bank/src/FloydTortoiseAndHareAlgorithm.java
@@ -15,16 +15,33 @@ public class FloydTortoiseAndHareAlgorithm {
         Time Complexity: O(n)
         Space Complexity: O(1)
      */
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-        ListNode slow = head;
-        ListNode fast = head.next;
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
 
-        while(slow != fast) {
-            if(fast == null || fast.next == null) return false;
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode intersect = getIntersection(head);
+        if(intersect == null) return null;
+
+        ListNode ptr1 = head;
+        ListNode ptr2 = intersect;
+
+        while(ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
         }
-        return true;
+        return ptr1;
+    }
+
+    private ListNode getIntersection(ListNode node) {
+        ListNode tortoise = node;
+        ListNode hare = node;
+
+        while(hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if(tortoise == hare) {
+                return tortoise;
+            }
+        }
+        return null;
     }
 }
